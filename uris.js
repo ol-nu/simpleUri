c91bd8353c7d7e053e02ab9354ddfecdb9f8eee9
olga-nudel <olga_nudel@arcor.de>
2015-12-14 23:26:09 +0100

    First commit

---
+++ uris.js
@@ -0,0 +1,65 @@
+/*
+Example of extraction of URIs for a object place
+*/
+
+$(document).ready(function(){
+
+   //This is to remove the validation message if no poster image is present
+
+   $('#city').focus(function(){
+  
+      var notEmpty = $("#uri");
+      console.log("notEmpty");
+      if(notEmpty == false){
+         $('#uri').empty();
+          
+      }
+   });
+
+   var getUri = function(){
+   		
+   		var classtype = "place";
+		var label = $('#city').val();
+
+         //Has the user entered anything?
+
+         if(label == ''){
+
+            //If the input field was empty
+
+            $('#uri').html("<h2 class='loading'>Please enter the city!</h2>");
+            } else {
+            	
+					$.ajax({
+						url: "http://lookup.dbpedia.org/api/search.asmx/KeywordSearch?QueryClass="+classtype+"&MaxHits=1&QueryString="+label,
+						dataType:    'xml',
+						timeout : 3000000,
+						success:     function(data) {
+								
+						var xml=data;
+						var $jQuerymyUris =$(xml).find("Result");
+									
+						var eid = $jQuerymyUris.find("URI:first").text();
+
+						var uri='<tr><td>'+classtype+'</td><td>'+label+'</td><td>'+eid+'</td></tr>';
+						$('#uri').html(uri);
+						$('#city').empty();
+
+						}//success
+				    	,
+						error: function(err) {
+						$('#uri').html("<h2 class='loading'>error loading "+label+"</h2>");
+						    	}
+						    });//Ajax xml
+					}
+				}	
+	$('#search').click(getUri);
+
+   $('#city').keyup(function(event){
+
+       if(event.keyCode == 13){
+
+           getUri();
+      }
+   })
+})
